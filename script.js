2c686e45984b40d5913caea9407fca770a5c0b70
= <=>
2024-11-08 13:41:28 +0500

    New update

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ let ViewMore = document.getElementById('ViewMore');
 let Back = document.getElementById('Back');
 let Back2 = document.getElementById('Back2');
 let Back3 = document.getElementById('Back3');
+let Back4 = document.getElementById('Back4');
 let ViewMore2 = document.getElementById('ViewMore2');
 let ViewMore3 = document.getElementById('ViewMore3');
+let ViewMore4 = document.getElementById('ViewMore4');
 let Search = document.getElementById('Search');
+let profile = document.getElementById('logo');
+let BackToHome = document.getElementById('BackToHome');
 
 
 ViewMore.addEventListener('click', () => {
@@ -23,6 +27,11 @@ ViewMore3.addEventListener('click', () => {
     document.body.querySelector('.container4').style.display = 'block'
 })
 
+ViewMore4.addEventListener('click', () => {
+    document.body.querySelector('.container').style.display = 'none'
+    document.body.querySelector('.container5').style.display = 'block'
+})
+
 Back.addEventListener('click', () => {
     document.body.querySelector('.container').style.display = 'block'
     document.body.querySelector('.container2').style.display = 'none'
@@ -39,3 +48,18 @@ Back3.addEventListener('click', () => {
 })
 
 
+Back4.addEventListener('click', () => {
+    document.body.querySelector('.container').style.display = 'block'
+    document.body.querySelector('.container5').style.display = 'none'
+})
+
+
+profile.addEventListener('click', () => {
+    document.body.querySelector('.container').style.display = 'none'
+    document.body.querySelector('.Profile').style.display = 'block'
+})
+
+BackToHome.addEventListener('click', () => {
+    document.body.querySelector('.container').style.display = 'block'
+    document.body.querySelector('.Profile').style.display = 'none'
+})
